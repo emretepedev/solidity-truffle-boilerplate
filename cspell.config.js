01f8176a1322dd98a3ee27907263b96de935e2fb
Emre Tepe <emretepedev@gmail.com>
2022-03-12 05:53:47 +0300

    added cspell and fixed cspell warnings

---
+++ cspell.config.js
@@ -0,0 +1,33 @@
+module.exports = {
+  version: '0.2',
+  language: 'en',
+  words: [
+    // general
+    'jscoverage',
+    'hdwallet',
+    // README.md
+    'emretepedev',
+    'SAST',
+    'makeareadme',
+    'Kubernetes',
+  ],
+  flagWords: [],
+  ignoreWords: [],
+  ignorePaths: ['yarn.lock'],
+  allowCompoundWords: true,
+  import: [],
+  dictionaries: [
+    'javascript',
+    'typescript',
+    'npm',
+    'node',
+    'filetypes',
+    'softwareTerms',
+    'bash',
+    'powershell',
+    'cpp',
+    'aws',
+    'public-licenses',
+    'cryptocurrencies',
+  ],
+};
