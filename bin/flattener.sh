13de63b307dc474e43ebab285b38a7217a01ea68
Emre Tepe <emretepedev@gmail.com>
2022-03-22 13:31:36 +0300

    fixed flattener.sh

--- bin/flattener.sh
+++ bin/flattener.sh
@@ -6,7 +6,4 @@ rm -rf solbin/flatten/"$1Flatten.sol" &>/dev/null
 
 yarn run truffle-flattener contracts/"$1".sol >>solbin/flatten/"$1Flatten".sol
 
-ex -c ':1,2d' -c ':wq' solbin/flatten/"$1Flatten".sol
-ex -snc '$-0,$d|x' solbin/flatten/"$1Flatten".sol
-
 echo "export solbin/flatten/$1Flatten.sol"
--- bin/solcrc.sh
+++ bin/solcrc.sh
@@ -6,6 +6,6 @@ if [ -f ".env" ]; then
   solc-select install "$TRUFFLE_SOLIDITY_VERSION"
   solc-select use "$TRUFFLE_SOLIDITY_VERSION"
 else
-  echo "Cannot find .env file."
+  echo "Cannot find .env file. Fix and re-run the 'solcrc.sh'."
   exit 0
 fi
