42088613468497442a8455e046ae52100dee239c
Emre Tepe <emretepedev@gmail.com>
2022-04-13 17:42:12 +0300

    updated generateFlatten.sh

--- bin/generateFlatten.sh
+++ bin/generateFlatten.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-mkdir solbin/ &>/dev/null
-mkdir solbin/flatten/ &>/dev/null
-rm -rf solbin/flatten/"$1Flatten.sol" &>/dev/null
+if [ ! -d solbin/flatten/ ]; then
+  mkdir solbin/flatten/ -pv
+fi
+
+if [ -f solbin/flatten/"$1Flatten.sol" ]; then
+  rm -rf solbin/flatten/"$1Flatten.sol"
+fi
 
 yarn run truffle-flattener contracts/"$1".sol >>solbin/flatten/"$1Flatten".sol
 
