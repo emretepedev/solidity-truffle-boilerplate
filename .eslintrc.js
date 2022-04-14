4edcf9304ae454d8606388bd39bafcdb130e920a
Emre Tepe <emretepedev@gmail.com>
2022-04-14 20:47:03 +0300

    updated eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,15 +2,35 @@ module.exports = {
   root: true,
 
   env: {
+    browser: false,
     es2021: true,
+    mocha: true,
+    node: true,
   },
 
-  extends: ['eslint:recommended', 'plugin:json/recommended', 'prettier'],
+  extends: [
+    'eslint:recommended',
+    'plugin:node/recommended',
+    'plugin:json/recommended',
+    'prettier',
+  ],
 
   plugins: ['json', 'prettier'],
 
   rules: {
     'prettier/prettier': 'warn',
-    'no-undef': 'off',
+    'node/no-unsupported-features/es-syntax': [
+      'error',
+      { ignores: ['modules'] },
+    ],
   },
+
+  overrides: [
+    {
+      files: ['test/*.test.js', 'migrations/*.js'],
+      rules: {
+        'no-undef': 'off',
+      },
+    },
+  ],
 };
