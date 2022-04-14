b3bc4851f340c58865ab1fdbe6663376c696ea89
Emre Tepe <emretepedev@gmail.com>
2022-04-14 20:37:05 +0300

    updated msg.sender to openzeppelin standarts

--- contracts/FooToken.sol
+++ contracts/FooToken.sol
@@ -6,7 +6,7 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract FooToken is ERC20 {
     constructor(uint256 initialSupply) ERC20("Foo Token", "FOO") {
-        _mint(msg.sender, initialSupply);
+        _mint(_msgSender(), initialSupply);
     }
 
     function decimals() public pure override returns (uint8) {
